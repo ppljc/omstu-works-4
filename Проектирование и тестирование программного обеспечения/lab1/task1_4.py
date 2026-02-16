9ec9cdd7cd84e22dd4ccaff245e1f00ec0e6ef8c
ppljc <tony_shmidt@mail.ru>
2026-02-16 17:40:35 +0600

    fix: pitpo lab1 task1_4 fixed when for candidate was no votes in first order

--- Проектирование и тестирование программного обеспечения/lab1/task1_4.py
+++ Проектирование и тестирование программного обеспечения/lab1/task1_4.py
@@ -7,14 +7,18 @@ def string_input() -> list:
     for _block in range(blocks_amount):
         n = int(input())  # candidates amount
         candidates = []  # for candidates names
+        candidate_votes_dict = {}  # votes from first order in form candidate: {votes, index}
 
         for i in range(n):  # candidates enter
             candidate = input()
             candidates.append(candidate)
+            candidate_votes_dict[candidate] = {
+                'votes': 0,
+                'index': i
+            }
 
         people_amount = 0
         people_votes = []
-        candidate_votes_dict = {}  # votes from first order in form candidate: {votes, index}
         candidate_votes = []  # votes from first order in form [id, candidate, votes]
 
         while True:
@@ -28,7 +32,7 @@ def string_input() -> list:
             people_votes.append(vote)
 
             candidate_votes_dict[candidates[vote[0]]] = {
-                'votes': candidate_votes_dict.get(candidates[vote[0]], {}).get('votes', 0) + 1,
+                'votes': candidate_votes_dict[candidates[vote[0]]]['votes'] + 1,
                 'index': vote[0]
             }
 
@@ -70,6 +74,7 @@ def calculate_votes(people_votes: list, candidate_votes: list, people_amount: in
         candidate_votes_cut = candidate_votes.copy()  # copying array
         candidate_votes_removed = []  # array for candidates who was removed due to the lowest votes
         candidate_votes_removed_indexes = []  # array for indexes of candidates from previous array
+
         for i in range(len(candidate_votes)):
             if candidate_votes[i]['votes'] == candidate_votes[0]['votes']:  # if candidate votes equals to minimum candidate votes
                 candidate_votes_removed.append(candidate_votes[i])  # add candidate to array
@@ -78,10 +83,13 @@ def calculate_votes(people_votes: list, candidate_votes: list, people_amount: in
 
         for i in range(len(candidate_votes_removed)):  # for every removed candidate
             index_removed = candidate_votes_removed[i]['index']  # index of candidate who was removed
+
             for k in range(len(people_votes)):  # for every people vote
                 people_vote = people_votes[k]  # take N people votes
+
                 if people_vote[0] == index_removed:  # if first vote from people in array of removed candidate indexes
                     new_people_vote = []  # array for new people vote
+
                     for j in range(len(people_vote[1:])):  # for every next vote from people after first
                         current_people_vote = people_vote[1:][j]  # new people vote
 
@@ -94,6 +102,7 @@ def calculate_votes(people_votes: list, candidate_votes: list, people_amount: in
                                 candidate_vote['votes'] = candidate_vote.get('votes', 0) + 1  # add vote to candidate
 
                         break  # break because next votes dont needed
+
                     people_votes[k] = new_people_vote  # updating people vote
 
         result = calculate_votes(people_votes, candidate_votes_cut, people_amount)  # recursion for calculating
