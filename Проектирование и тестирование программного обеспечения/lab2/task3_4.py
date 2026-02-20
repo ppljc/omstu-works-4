00d83f95d004a5a6c2fd9ecb06d96499772003b0
ppljc <tony_shmidt@mail.ru>
2026-02-20 13:59:02 +0600

    feat: pitpo lab2 task3_4 found substring only in current order

--- Проектирование и тестирование программного обеспечения/lab2/task3_4.py
+++ Проектирование и тестирование программного обеспечения/lab2/task3_4.py
@@ -1,5 +1,18 @@
 def task3_4() -> None:
-    pass
+    """
+    Function to calculate Australian voting
+    :return: None
+    """
+    print('Входные данные:')
+    blocks = blocks_input()
+
+    print('Выходные данные:')
+    for block in blocks:
+        word_a, word_b = block
+
+        substring = found_matching_substring(word_a, word_b)
+
+        print(substring)
 
 
 def blocks_input() -> list[list]:
@@ -7,8 +20,8 @@ def blocks_input() -> list[list]:
     block = []
 
     while True:
-        if len(block) > 2:
-            blocks.append(block)
+        if len(block) == 2:
+            blocks.append(tuple(block))
             block = []
 
         word = input()
@@ -21,5 +34,25 @@ def blocks_input() -> list[list]:
     return blocks
 
 
-def found_substring():
-    pass
+def found_matching_substring(word_a: str, word_b: str) -> str:
+    result = ''
+
+    max_length = len(word_a)
+
+    for start in range(0, max_length, 1):
+        for length in range(1, max_length, 1):
+            end = start + length
+
+            if end > max_length:
+                end = max_length
+
+            substring = word_a[start:end]
+
+            if substring in word_b and len(substring) >= len(result):
+                result = substring
+
+    return result
+
+
+if __name__ == '__main__':
+    task3_4()
